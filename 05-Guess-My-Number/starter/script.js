c3c466fcb7f4a42925c9d2461a0f64c83f4fac7a
Daniel dos Santos Araujo <denenewton@icloud.com>
2021-02-10 12:09:44 -0300

    finalização do game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -2,10 +2,22 @@
 
 let secretNumber = Math.floor(Math.random() * 20 + 1);
 let $ = document.querySelector.bind(this.document);
-
+let highscore = 0;
 console.log(secretNumber);
 
 $('.check').addEventListener('click', tentativa);
+
+$('.again').addEventListener('click', again);
+
+function again() {
+  secretNumber = Math.floor(Math.random() * 20 + 1);
+  $('.score').textContent = '20';
+  $('.message').textContent = 'Start guessing...';
+  $('.number').textContent = '?';
+  $('body').style.backgroundColor = '#222';
+  $('.number').style.width = '15rem';
+  $('.guess').value = '';
+}
 
 function tentativa() {
   const gess = Number($('.guess').value);
@@ -15,7 +27,13 @@ function tentativa() {
     $('.message').textContent = '🚫 No number';
   } else if (gess === secretNumber) {
     $('.number').textContent = `${secretNumber}`;
+    if(highscore < score){
+      highscore = score;
+      $('.highscore').textContent = `${highscore}`;
+    }
     $('.message').textContent = '👍 Correct number';
+    $('body').style.backgroundColor = ' #60b347';
+    $('.number').style.width = '30rem';
   } else if (gess > secretNumber) {
     if (score > 1) {
       $('.message').textContent = '📈 Too High!';
@@ -23,6 +41,8 @@ function tentativa() {
     } else {
       $('.message').textContent = '😩 Game Over!';
       $('.score').textContent = `${score - 1}`;
+      highscore = 0;
+      $('.highscore').textContent = `${highscore}`;
     }
   } else if (gess < secretNumber) {
     if (score > 1) {
